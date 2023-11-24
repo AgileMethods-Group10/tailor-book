863270f9bcd7e97bd19caad23d7523f845f8176d
baabashinelle <baabadampare@gmail.com>
2023-11-24 09:02:50 +0000

    initial commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>This is main file. Components will be called here. so build your components in the components folder and come and call them here. make sure to group code. refer to docs if anything. happy coding</Text>
       <StatusBar style="auto" />
     </View>
   );
