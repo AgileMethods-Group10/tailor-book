abd8d6940fd4691b9275c91703b249187ad2cf4d
baabashinelle <baabadampare@gmail.com>
2024-01-06 16:36:27 +0000

    collection

--- screens/Collection.js
+++ screens/Collection.js
@@ -1,8 +1,16 @@
 import React from "react";
-import { View, Text, StyleSheet, Image, ScrollView } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
 import Navbar from "../components/Navbar";
 import { useFonts } from "expo-font";
 import BottomNavbar from "../components/BottomNavigation";
+import SVG from "../assets/pyjamas.svg";
 
 const Collection = () => {
   const [fontsLoaded] = useFonts({
@@ -16,76 +24,124 @@ const Collection = () => {
       <Navbar />
       <ScrollView>
         <View style={styles.dressContainer}>
-          <Image
-            source={require("../assets/dress1.png")}
-            style={{ width: 100, height: 150 }}
-          />
-          <Image
-            source={require("../assets/dress2.png")}
-            style={{ width: 100, height: 150 }}
-          />
-        </View>
-        <View style={styles.textContainer}>
-          <Text style={{ fontFamily: "Poppins-Medium", fontSize: 16 }}>
-            Blouse
-          </Text>
-          <Text style={{ fontFamily: "Poppins-Medium", fontSize: 16 }}>
-            Dress
-          </Text>
-        </View>
-        <View style={styles.dressContainer}>
-          <Image
-            source={require("../assets/dress1.png")}
-            style={{ width: 100, height: 150 }}
-          />
-          <Image
-            source={require("../assets/dress2.png")}
-            style={{ width: 100, height: 150 }}
-          />
-        </View>
-        <View style={styles.textContainer}>
-          <Text style={{ fontFamily: "Poppins-Medium", fontSize: 16 }}>
-            Blouse
-          </Text>
-          <Text style={{ fontFamily: "Poppins-Medium", fontSize: 16 }}>
-            Dress
-          </Text>
+          <View style={styles.subContainer}>
+            <TouchableOpacity>
+              <Image
+                source={require("../assets/dress1.png")}
+                style={{ width: 100, height: 150 }}
+              />
+            </TouchableOpacity>
+            <Text style={{ fontFamily: "Poppins-Medium", fontSize: 16 }}>
+              Black Jalabia
+            </Text>
+          </View>
+          <View style={styles.subContainer}>
+            <TouchableOpacity>
+              <Image
+                source={require("../assets/dress2.png")}
+                style={{ width: 100, height: 150 }}
+              />
+            </TouchableOpacity>
+            <Text style={{ fontFamily: "Poppins-Medium", fontSize: 16 }}>
+              Straight Dress
+            </Text>
+          </View>
         </View>
         <View style={styles.dressContainer}>
-          <Image
-            source={require("../assets/dress1.png")}
-            style={{ width: 100, height: 150 }}
-          />
-          <Image
-            source={require("../assets/dress2.png")}
-            style={{ width: 100, height: 150 }}
-          />
-        </View>
-        <View style={styles.textContainer}>
-          <Text style={{ fontFamily: "Poppins-Medium", fontSize: 16 }}>
-            Blouse
-          </Text>
-          <Text style={{ fontFamily: "Poppins-Medium", fontSize: 16 }}>
-            Dress
-          </Text>
+          <View style={styles.subContainer}>
+            <TouchableOpacity>
+            <Image
+                source={require("../assets/gown.png")}
+                style={{ width: 100, height: 150 }}
+              />
+            </TouchableOpacity>
+            <Text style={{ fontFamily: "Poppins-Medium", fontSize: 16 }}>
+              Gown
+            </Text>
+          </View>
+          <View style={styles.subContainer}>
+            <TouchableOpacity>
+              <Image
+                source={require("../assets/pyjamas.png")}
+                style={{ width: 100, height: 150 }}
+              />
+            </TouchableOpacity>
+            <Text style={{ fontFamily: "Poppins-Medium", fontSize: 16 }}>
+              Pyjamas
+            </Text>
+          </View>
         </View>
         <View style={styles.dressContainer}>
-          <Image
-            source={require("../assets/dress1.png")}
-            style={{ width: 100, height: 150 }}
-          />
-          <Image
-            source={require("../assets/dress2.png")}
-            style={{ width: 100, height: 150 }}
-          />
+          <View style={styles.subContainer}>
+            <TouchableOpacity>
+            <Image
+                source={require("../assets/blouse.png")}
+                style={{ width: 100, height: 150 }}
+              />
+            </TouchableOpacity>
+            <Text style={{ fontFamily: "Poppins-Medium", fontSize: 16 }}>
+              Blouse
+            </Text>
+          </View>
+          <View style={styles.subContainer}>
+            <TouchableOpacity>
+              <Image
+                source={require("../assets/indian-wear.png")}
+                style={{ width: 100, height: 150 }}
+              />
+            </TouchableOpacity>
+            <Text style={{ fontFamily: "Poppins-Medium", fontSize: 16 }}>
+              Indian Wear
+            </Text>
+          </View>
         </View>
-        <View style={styles.textContainer}>
-          <Text style={{ fontFamily: "Poppins-Medium", fontSize: 16 }}>
-            Blouse
-          </Text>
-          <Text style={{ fontFamily: "Poppins-Medium", fontSize: 16 }}>
-            Dress
-          </Text>
+        <View style={styles.dressContainer}>
+          <View style={styles.subContainer}>
+            <TouchableOpacity>
+            <Image
+                source={require("../assets/jumpsuit.png")}
+                style={{ width: 100, height: 150 }}
+              />
+            </TouchableOpacity>
+            <Text style={{ fontFamily: "Poppins-Medium", fontSize: 16 }}>
+              Jumpsuit
+            </Text>
+          </View>
+          <View style={styles.subContainer}>
+            <TouchableOpacity>
+              <Image
+                source={require("../assets/pleated-dress.png")}
+                style={{ width: 100, height: 150 }}
+              />
+            </TouchableOpacity>
+            <Text style={{ fontFamily: "Poppins-Medium", fontSize: 16 }}>
+              Pleated Dress
+            </Text>
+          </View>
+        </View>
+        <View style={styles.dressContainer}>
+          <View style={styles.subContainer}>
+            <TouchableOpacity>
+            <Image
+                source={require("../assets/skirt.png")}
+                style={{ width: 100, height: 150 }}
+              />
+            </TouchableOpacity>
+            <Text style={{ fontFamily: "Poppins-Medium", fontSize: 16 }}>
+              Skirt
+            </Text>
+          </View>
+          <View style={styles.subContainer}>
+            <TouchableOpacity>
+              <Image
+                source={require("../assets/suit.png")}
+                style={{ width: 100, height: 150 }}
+              />
+            </TouchableOpacity>
+            <Text style={{ fontFamily: "Poppins-Medium", fontSize: 16 }}>
+              Suit
+            </Text>
+          </View>
         </View>
       </ScrollView>
     </View>
@@ -98,14 +154,8 @@ const styles = StyleSheet.create({
     height: "100%",
     paddingTop: 80,
     paddingHorizontal: 30,
-    backgroundColor: "#fff",
+    backgroundColor: "#e5e5e5",
     fontFamily: "Poppins-Regular",
-  },
-  welcome: {
-    height: "80%",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
   },
   dressContainer: {
     display: "flex",
@@ -113,16 +163,17 @@ const styles = StyleSheet.create({
     justifyContent: "space-around",
     alignItems: "center",
     paddingVertical: 20,
-    backgroundColor: "#e5e5e5",
-    borderRadius: 10,
-    marginTop: 40,
+    marginTop: 30,
+    backgroundColor: "#fff",
+    borderRadius: "10px",
+    gap: "8px",
   },
-  textContainer: {
+  subContainer: {
     display: "flex",
-    flexDirection: "row",
-    justifyContent: "space-around",
+    flexDirection: "column",
+    textAlign: "center",
     alignItems: "center",
-    marginVertical: 20,
+    gap: "18px",
   },
 });
 export default Collection;
