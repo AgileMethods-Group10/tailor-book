e2d1048b4fdf7cba763f951246d9a49eec846bd7
baabashinelle <baabadampare@gmail.com>
2024-01-11 16:17:51 +0000

    add measurements form

--- screens/AddClient.js
+++ screens/AddClient.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   View,
   Text,
@@ -7,12 +7,23 @@ import {
   TouchableOpacity,
   ScrollView,
   TextInput,
+  Switch,
 } from "react-native";
 import ClientNav from "../components/ClientNav";
 import { useFonts } from "expo-font";
 import { useNavigation } from "@react-navigation/native";
+import {
+  Calendar,
+  CalendarList,
+  Agenda,
+  LocaleConfig,
+} from "react-native-calendars";
 
 const AddClient = () => {
+  const [selectedDate, setSelectedDate] = useState(false);
+  const [remindDate, setRemindDate] = useState(false);
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
   const [fontsLoaded] = useFonts({
     "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
     "Poppins-Medium": require("../assets/fonts/Poppins-Medium.ttf"),
@@ -141,29 +152,576 @@ const AddClient = () => {
             Fabrics Types:
           </Text>
           <ScrollView horizontal={true}>
-            <View style={{display: "flex", flexDirection: "row", gap: 20}}>
+            <View style={{ display: "flex", flexDirection: "row", gap: 20 }}>
               <Image
                 source={require("../assets/cloth1.png")}
                 style={{ width: 230, height: 230 }}
               />
-               <Image
+              <Image
                 source={require("../assets/cloth2.png")}
                 style={{ width: 230, height: 230 }}
               />
-               <Image
+              <Image
                 source={require("../assets/cloth3.png")}
                 style={{ width: 230, height: 230 }}
               />
-               <Image
+              <Image
                 source={require("../assets/cloth4.png")}
                 style={{ width: 230, height: 230 }}
               />
-               <Image
+              <Image
                 source={require("../assets/cloth5.png")}
                 style={{ width: 230, height: 230 }}
               />
             </View>
           </ScrollView>
+        </View>
+
+        <View style={styles.firstContainer}>
+          <Text
+            style={{
+              fontSize: 18,
+              fontFamily: "Poppins-Regular",
+              letterSpacing: 1,
+              marginVertical: 20,
+            }}
+          >
+            Delivery Date:
+          </Text>
+          <Calendar
+            onDayPress={(day) => {
+              setSelectedDate(day.dateString);
+            }}
+            markedDates={{
+              [selectedDate]: {
+                selected: true,
+                disableTouchEvent: true,
+                selectedDotColor: "orange",
+              },
+            }}
+            style={{
+              width: "100%",
+              paddingVertical: 10,
+            }}
+          />
+        </View>
+
+        <View style={styles.firstContainer}>
+          <Text
+            style={{
+              fontSize: 18,
+              fontFamily: "Poppins-Regular",
+              letterSpacing: 1,
+              marginVertical: 20,
+            }}
+          >
+            Remind Date: (Set a reminder)
+          </Text>
+          <Calendar
+            onDayPress={(day) => {
+              setRemindDate(day.dateString);
+            }}
+            markedDates={{
+              [remindDate]: {
+                selected: true,
+                disableTouchEvent: true,
+                selectedDotColor: "orange",
+              },
+            }}
+            style={{
+              width: "100%",
+              paddingVertical: 10,
+            }}
+          />
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <Text
+              style={{
+                fontSize: 16,
+                fontFamily: "Poppins-Regular",
+                letterSpacing: 1,
+              }}
+            >
+              Mark as urgent
+            </Text>
+            <Switch
+              trackColor={{ false: "#767577", true: "#81b0ff" }}
+              thumbColor={isEnabled ? "#e5e5e5" : "#f4f3f4"}
+              ios_backgroundColor="#3e3e3e"
+              onValueChange={toggleSwitch}
+              value={isEnabled}
+            />
+          </View>
+        </View>
+
+        <View style={styles.firstContainer}>
+          <Text
+            style={{
+              fontSize: 18,
+              fontFamily: "Poppins-Regular",
+              letterSpacing: 1,
+              marginVertical: 20,
+            }}
+          >
+            Add Measurement:
+          </Text>
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
+              paddingVertical: 10,
+            }}
+          >
+            <Text
+              style={{
+                fontSize: 16,
+                fontFamily: "Poppins-Regular",
+                letterSpacing: 1,
+                width: 100,
+              }}
+            >
+              Length
+            </Text>
+            <TextInput
+              placeholder="00"
+              style={{
+                fontSize: 18,
+                fontFamily: "Poppins-Regular",
+                letterSpacing: 1,
+                borderBottomWidth: 1,
+                borderBottomColor: "#000",
+                width: 100,
+                textAlign: "center",
+              }}
+            />
+            <Text
+              style={{
+                fontSize: 16,
+                fontFamily: "Poppins-Regular",
+                letterSpacing: 1,
+              }}
+            >
+              inches
+            </Text>
+          </View>
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
+              paddingVertical: 10,
+            }}
+          >
+            <Text
+              style={{
+                fontSize: 16,
+                fontFamily: "Poppins-Regular",
+                letterSpacing: 1,
+                width: 100,
+              }}
+            >
+              Shoulder
+            </Text>
+            <TextInput
+              placeholder="00"
+              style={{
+                fontSize: 18,
+                fontFamily: "Poppins-Regular",
+                letterSpacing: 1,
+                borderBottomWidth: 1,
+                borderBottomColor: "#000",
+                width: 100,
+                textAlign: "center",
+              }}
+            />
+            <Text
+              style={{
+                fontSize: 16,
+                fontFamily: "Poppins-Regular",
+                letterSpacing: 1,
+              }}
+            >
+              inches
+            </Text>
+          </View>
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
+              paddingVertical: 10,
+            }}
+          >
+            <Text
+              style={{
+                fontSize: 16,
+                fontFamily: "Poppins-Regular",
+                letterSpacing: 1,
+                width: 100,
+              }}
+            >
+              Chest
+            </Text>
+            <TextInput
+              placeholder="00"
+              style={{
+                fontSize: 18,
+                fontFamily: "Poppins-Regular",
+                letterSpacing: 1,
+                borderBottomWidth: 1,
+                borderBottomColor: "#000",
+                width: 100,
+                textAlign: "center",
+              }}
+            />
+            <Text
+              style={{
+                fontSize: 16,
+                fontFamily: "Poppins-Regular",
+                letterSpacing: 1,
+              }}
+            >
+              inches
+            </Text>
+          </View>
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
+              paddingVertical: 10,
+            }}
+          >
+            <Text
+              style={{
+                fontSize: 16,
+                fontFamily: "Poppins-Regular",
+                letterSpacing: 1,
+                width: 100,
+              }}
+            >
+              Waist
+            </Text>
+            <TextInput
+              placeholder="00"
+              style={{
+                fontSize: 18,
+                fontFamily: "Poppins-Regular",
+                letterSpacing: 1,
+                borderBottomWidth: 1,
+                borderBottomColor: "#000",
+                width: 100,
+                textAlign: "center",
+              }}
+            />
+            <Text
+              style={{
+                fontSize: 16,
+                fontFamily: "Poppins-Regular",
+                letterSpacing: 1,
+              }}
+            >
+              inches
+            </Text>
+          </View>
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
+              paddingVertical: 10,
+            }}
+          >
+            <Text
+              style={{
+                fontSize: 16,
+                fontFamily: "Poppins-Regular",
+                letterSpacing: 1,
+                width: 100,
+              }}
+            >
+              Hips
+            </Text>
+            <TextInput
+              placeholder="00"
+              style={{
+                fontSize: 18,
+                fontFamily: "Poppins-Regular",
+                letterSpacing: 1,
+                borderBottomWidth: 1,
+                borderBottomColor: "#000",
+                width: 100,
+                textAlign: "center",
+              }}
+            />
+            <Text
+              style={{
+                fontSize: 16,
+                fontFamily: "Poppins-Regular",
+                letterSpacing: 1,
+              }}
+            >
+              inches
+            </Text>
+          </View>
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
+              paddingVertical: 10,
+            }}
+          >
+            <Text
+              style={{
+                fontSize: 16,
+                fontFamily: "Poppins-Regular",
+                letterSpacing: 1,
+                width: 100,
+              }}
+            >
+              Arm Length
+            </Text>
+            <TextInput
+              placeholder="00"
+              style={{
+                fontSize: 18,
+                fontFamily: "Poppins-Regular",
+                letterSpacing: 1,
+                borderBottomWidth: 1,
+                borderBottomColor: "#000",
+                width: 100,
+                textAlign: "center",
+              }}
+            />
+            <Text
+              style={{
+                fontSize: 16,
+                fontFamily: "Poppins-Regular",
+                letterSpacing: 1,
+              }}
+            >
+              inches
+            </Text>
+          </View>
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
+              paddingVertical: 10,
+            }}
+          >
+            <Text
+              style={{
+                fontSize: 16,
+                fontFamily: "Poppins-Regular",
+                letterSpacing: 1,
+                width: 100,
+              }}
+            >
+              Around Arm
+            </Text>
+            <TextInput
+              placeholder="00"
+              style={{
+                fontSize: 18,
+                fontFamily: "Poppins-Regular",
+                letterSpacing: 1,
+                borderBottomWidth: 1,
+                borderBottomColor: "#000",
+                width: 100,
+                textAlign: "center",
+              }}
+            />
+            <Text
+              style={{
+                fontSize: 16,
+                fontFamily: "Poppins-Regular",
+                letterSpacing: 1,
+              }}
+            >
+              inches
+            </Text>
+          </View>
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
+              paddingVertical: 10,
+            }}
+          >
+            <Text
+              style={{
+                fontSize: 16,
+                fontFamily: "Poppins-Regular",
+                letterSpacing: 1,
+                width: 100,
+              }}
+            >
+              Wrist
+            </Text>
+            <TextInput
+              placeholder="00"
+              style={{
+                fontSize: 18,
+                fontFamily: "Poppins-Regular",
+                letterSpacing: 1,
+                borderBottomWidth: 1,
+                borderBottomColor: "#000",
+                width: 100,
+                textAlign: "center",
+              }}
+            />
+            <Text
+              style={{
+                fontSize: 16,
+                fontFamily: "Poppins-Regular",
+                letterSpacing: 1,
+              }}
+            >
+              inches
+            </Text>
+          </View>
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
+              paddingVertical: 10,
+            }}
+          >
+            <Text
+              style={{
+                fontSize: 16,
+                fontFamily: "Poppins-Regular",
+                letterSpacing: 1,
+                width: 100,
+              }}
+            >
+              Collar Front
+            </Text>
+            <TextInput
+              placeholder="00"
+              style={{
+                fontSize: 18,
+                fontFamily: "Poppins-Regular",
+                letterSpacing: 1,
+                borderBottomWidth: 1,
+                borderBottomColor: "#000",
+                width: 100,
+                textAlign: "center",
+              }}
+            />
+            <Text
+              style={{
+                fontSize: 16,
+                fontFamily: "Poppins-Regular",
+                letterSpacing: 1,
+              }}
+            >
+              inches
+            </Text>
+          </View>
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
+              paddingVertical: 10,
+            }}
+          >
+            <Text
+              style={{
+                fontSize: 16,
+                fontFamily: "Poppins-Regular",
+                letterSpacing: 1,
+                width: 100,
+              }}
+            >
+              Collar Back
+            </Text>
+            <TextInput
+              placeholder="00"
+              style={{
+                fontSize: 18,
+                fontFamily: "Poppins-Regular",
+                letterSpacing: 1,
+                borderBottomWidth: 1,
+                borderBottomColor: "#000",
+                width: 100,
+                textAlign: "center",
+              }}
+            />
+            <Text
+              style={{
+                fontSize: 16,
+                fontFamily: "Poppins-Regular",
+                letterSpacing: 1,
+              }}
+            >
+              inches
+            </Text>
+          </View>
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
+              paddingVertical: 10,
+            }}
+          >
+            <Text
+              style={{
+                fontSize: 16,
+                fontFamily: "Poppins-Regular",
+                letterSpacing: 1,
+                width: 100,
+              }}
+            >
+              Around Leg
+            </Text>
+            <TextInput
+              placeholder="00"
+              style={{
+                fontSize: 18,
+                fontFamily: "Poppins-Regular",
+                letterSpacing: 1,
+                borderBottomWidth: 1,
+                borderBottomColor: "#000",
+                width: 100,
+                textAlign: "center",
+              }}
+            />
+            <Text
+              style={{
+                fontSize: 16,
+                fontFamily: "Poppins-Regular",
+                letterSpacing: 1,
+              }}
+            >
+              inches
+            </Text>
+          </View>
         </View>
       </ScrollView>
     </View>
