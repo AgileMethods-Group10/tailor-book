53050e4e31714db67b8949b317cee8d487dffef2
baabashinelle <baabadampare@gmail.com>
2024-01-12 01:29:56 +0000

    add client profilee

---
+++ components/PaymentNav.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+import { StyleSheet } from "react-native";
+import { Icon } from "react-native-elements";
+import { useFonts } from "expo-font";
+import { useNavigation } from "@react-navigation/native";
+
+const PaymentNav = () => {
+  const [fontsLoaded] = useFonts({
+    "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
+    "Poppins-Medium": require("../assets/fonts/Poppins-Medium.ttf"),
+    "Poppins-Bold": require("../assets/fonts/Poppins-Bold.ttf"),
+  });
+  const navigation = useNavigation();
+  return (
+    <View style={styles.container}>
+      <View style={styles.back}>
+        <TouchableOpacity>
+          <Icon name="arrow-left" size={30} color="#1676F3" />
+        </TouchableOpacity>
+      </View>
+      <Text style={styles.text}>Payment Summary</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignContent: "center",
+  },
+  back: {
+    borderColor: "#e5e5e5",
+    borderWidth: 2,
+    borderRadius: 5,
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: "600",
+    fontFamily: "Poppins-Regular",
+    letterSpacing: -0.34,
+  },
+  span: {
+    fontWeight: "300",
+    fontFamily: "Poppins-Regular",
+  },
+});
+export default PaymentNav;
--- screens/Bill.js
+++ screens/Bill.js
@@ -1,10 +1,110 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView, TouchableOpacity } from "react-native";
+import React from "react";
+import { Icon } from "react-native-elements";
+import { useNavigation } from "@react-navigation/native";
+import PaymentNav from "../components/PaymentNav";
 
 export default function Bill() {
+  const navigation = useNavigation();
   return (
-    <View>
-      <Text>Bill</Text>
-    </View>
-  )
+    <SafeAreaView>
+      <View style={{ paddingHorizontal: 20, paddingVertical: 30 }}>
+        <PaymentNav />
+
+        <View
+          style={{
+            backgroundColor: "#fff",
+            marginVertical: 20,
+            paddingVertical: 20,
+            paddingHorizontal: 15,
+            borderRadius: 10,
+          }}
+        >
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
+              padding: 12,
+              borderColor: "#e5e5e5",
+              borderWidth: 2,
+              borderRadius: 5,
+              marginTop: 10,
+            }}
+          >
+            <Text
+              style={{
+                fontSize: 16,
+                fontFamily: "Poppins-Regular",
+                letterSpacing: 1,
+              }}
+            >
+              Material Kaftan
+            </Text>
+            <Text
+              style={{
+                fontSize: 16,
+                fontFamily: "Poppins-Bold",
+                letterSpacing: 1,
+                color: "#bbb",
+              }}
+            >
+              GHC 200.00    
+            </Text>
+          </View>
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
+              paddingTop: 30,
+            }}
+          >
+            <Text
+              style={{
+                fontSize: 16,
+                fontFamily: "Poppins-Regular",
+                letterSpacing: 1,
+              }}
+            >
+             Total:
+            </Text>
+            <Text
+              style={{
+                fontSize: 16,
+                fontFamily: "Poppins-Bold",
+                letterSpacing: 1,
+                color: "#bbb",
+              }}
+            >
+              GHC 200.00
+            </Text>
+          </View>
+
+          <View style={{ marginTop: 50 }}>
+            <TouchableOpacity
+              style={{
+                backgroundColor: "#1676F3",
+                paddingVertical: 14,
+                borderRadius: 5,
+              }}
+              onPress={() => navigation.navigate("tabs")}
+            >
+              <Text
+                style={{
+                  fontFamily: "Poppins-Medium",
+                  color: "#fff",
+                  textAlign: "center",
+                }}
+              >
+                Return to Home
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
 }
